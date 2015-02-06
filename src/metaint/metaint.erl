eb0d169f640c7ff9c4eb651b4bcf2ea8f0d593da
Emil Tullstedt <sakjur@gmail.com>
2015-02-06 10:30:57 +0100

    Added dummy metainterpreter

--- src/metaint/metaint.erl
+++ src/metaint/metaint.erl
@@ -1,6 +1,6 @@
 -module(metaint).
 
--export([eval_expr/2]).
+-export([eval/1]).
 
 eval_expr({atm, Id}, _) ->
     {ok, Id};
@@ -32,4 +32,46 @@ find_in_env(Var, [{HVar, HContent} | TEnv]) ->
         _ ->
             find_in_env(Var, TEnv)
     end.
+
+eval_match(ignore, _, Env) ->
+    {ok, Env};
+eval_match({atm, Id}, Id, Env) ->
+    {ok, Env};
+eval_match({var, Id}, Var, Env) ->
+    case find_in_env(Id, Env) of
+        false ->
+            {ok, [{Id, Var} | Env ]};
+        {Id, Var} ->
+            {ok, Env};
+        {Id, _} ->
+            fail
+    end;
+
+eval_match({cons, Curr, Next}, {H, T}, Env) ->
+    case eval_match(Curr, H, Env) of
+        fail ->
+            fail;
+        {ok, NEnv} ->
+            eval_match(Next, T, NEnv)
+    end;
+eval_match(_, _, _) ->
+    fail.
+
+eval_seq([Exp], Env) ->
+    eval_expr(Exp, Env);
+eval_seq([{match, Ptr, Exp} | Seq], Env) ->
+    case eval_expr(Exp, Env) of
+        error ->
+            error;
+        {ok, Str} ->
+            case eval_match(Ptr, Str, Env) of
+                fail ->
+                    error;
+                {ok, NEnv} ->
+                    eval_seq(Seq, NEnv)
+            end
+    end.
+
+eval(Seq) ->
+    eval_seq(Seq, []).
 
