64edb0a615fdcf1f9e16ec984c2e0319747bc4fc
Emil Tullstedt <sakjur@gmail.com>
2015-02-10 21:14:39 +0100

    Changed from 3 to 5 philosophers

--- src/philosophers/dinner.erl
+++ src/philosophers/dinner.erl
@@ -8,11 +8,17 @@ init() ->
     C1 = chopstick:start(),
     C2 = chopstick:start(),
     C3 = chopstick:start(),
+    C4 = chopstick:start(),
+    C5 = chopstick:start(),
+    
     Ctrl = self(),
     philosopher:start(5, C1, C2, "Arendt", Ctrl),
     philosopher:start(5, C2, C3, "Hypatia", Ctrl),
-    philosopher:start(5, C3, C1, "Simone", Ctrl),
-    wait(5, [C1, C2, C3]).
+    philosopher:start(5, C3, C4, "Simone", Ctrl),
+    philosopher:start(5, C4, C5, "Elizabeth", Ctrl),
+    philosopher:start(5, C5, C1, "Ayn", Ctrl),
+    Chopsticks = [C1, C2, C3, C4, C5],
+    wait(length(Chopsticks), Chopsticks).
 
 wait(0, Chopsticks) ->
     lists:foreach(fun(C) -> chopstick:quit(C) end, Chopsticks);
